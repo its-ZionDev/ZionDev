79b14ce82a4ff61886b23fc6bb0692acff6ffae0
its-ZionDev <itscreativezion@gmail.com>
2024-07-11 20:13:21 +0530

    Server optimization and route fixing

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ env.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
